9ce85907a5d3da5a73adf72b38a752bc98bb2411
faraz12948 <faraz15-12948@diu.edu.bd>
2020-12-20 19:19:53 +0600

    added postgreSql

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ToDO',
+        'USER': 'postgres',
+        'PASSWORD': 'faraz12948',
+        'HOST': 'localhost'
     }
 }
 
